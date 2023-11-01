f1b2842f1254d497cb08bedd658468d2d19ef6c9
Oliver Hjermitslev <oliv0989@gmail.com>
2023-11-01 14:06:39 +0100

    initial commit

---
+++ torch_trandsforms/__init__.py
@@ -0,0 +1,19 @@
+# type: ignore[attr-defined]
+"""A pytorch-first transform library for ND data, such as multi-channel 3D volumes"""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
