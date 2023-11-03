0446170bc450e638cf7de3af6fbacea528e7e037
Oliver Hjermitslev <oliv0989@gmail.com>
2023-11-03 10:14:29 +0100

    added compose and test

---
+++ tests/test_structure.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+
+from torch_trandsforms.rotation import RandomRotate90
+from torch_trandsforms.structure import Compose
+from torch_trandsforms.value import UniformNoise
+
+
+def test_compose():
+    """test Compose"""
+
+    transform = Compose([RandomRotate90(), UniformNoise()])
+
+    assert "RandomRotate90" in str(transform)
+    assert "UniformNoise" in str(transform)
+
+    tensor = torch.arange(16, dtype=torch.float).view(2, 2, 2, 2)
+    transformed = transform(tensor=tensor)["tensor"]
+
+    assert transformed.shape == (2, 2, 2, 2)
+    assert transformed.dtype == torch.float
---
+++ torch_trandsforms/structure.py
@@ -0,0 +1,41 @@
+import torch
+
+__all__ = ["Compose"]
+
+
+class Compose:
+    """
+    Composes several transforms together.
+    Please note that this class only automates the running of a transform pipeline.
+        The dimensionality and operating keys is left to the user, as they are rarely
+        assumed to be equal for all transform objects.
+
+    Args:
+        transforms (list of `transform` objects): list of transforms to compose.
+
+    Example:
+        >>> torch_trandsforms.Compose([
+        >>>    torch_trandsforms.RandomRotate90(p=0.66, nd=3),
+        >>>    torch_trandsforms.UniformNoise(p=1., low=0, hi=0.1, keys=["data", "extra_data"])
+        >>> ])
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, **inputs):
+        for transform in self.transforms:
+            inputs = transform(**inputs)
+        return inputs
+
+    def __repr__(self) -> str:
+        """stolen from torchvision"""
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            try:
+                format_string += f"    {t}"
+            except AttributeError:
+                format_string += f"    {t.__class__.__name__}"
+        format_string += "\n)"
+        return format_string
